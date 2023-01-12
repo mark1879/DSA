e250e5a0372cc6138e19825ff7b1fe0b3dfd5e35
mark1879 <mark1879@163.com>
2023-01-12 17:10:51 +0800

    add more cases to test removal

--- AVLTree/test_avl_tree.cpp
+++ AVLTree/test_avl_tree.cpp
@@ -7,56 +7,60 @@ enum class Implementation
     Nonrecursion
 };
 
-void CheckInOrder(const AVLTree<int>* tree, const std::vector<int>& keys)
-{
-    std::vector<int> in_order_keys;
-    tree->InOrder(in_order_keys);
 
-    for (int i = 0, size = (int)keys.size(); i < size; i++) 
-        EXPECT_EQ(in_order_keys[i], keys[i]);
+void CheckInOrder(const AVLTree<int>* tree, const std::vector<int>& source_in_order_keys)
+{
+    std::vector<int> keys;
+    tree->InOrder(keys);
+
+    EXPECT_EQ(source_in_order_keys.size(), keys.size());
+
+    for (int i = 0, size = (int)keys.size(); i < size; i++) {
+        EXPECT_EQ(source_in_order_keys[i], keys[i]);
+    }
 }
 
-void RemoveKey(AVLTree<int>* tree, std::vector<int>& keys, int key)
-{
-    tree->Remove(key);
-    EXPECT_EQ(tree->Query(key), false);
-    keys.erase(std::find(keys.begin(), keys.end(), key));
-    CheckInOrder(tree, keys);
-    EXPECT_EQ(tree->IsBalance(), true);
-}
 
-void TestRemoval(Implementation impl)
-{   
-    std::vector<int> keys = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+void TestRemoval(std::vector<int> source_keys, std::vector<int> source_in_order_keys, int reomve_key, Implementation impl)
+{
+    EXPECT_EQ(source_keys.size(), source_in_order_keys.size());
 
     AVLTree<int>* tree = nullptr;
     if (impl == Implementation::Recursion)
         tree = new RAVLTree<int>();
 
-    for (auto key : keys)
-        tree->Insert(key);
-    
-    RemoveKey(tree, keys, 1);
-    RemoveKey(tree, keys, 2);
-    RemoveKey(tree, keys, 6);
-    RemoveKey(tree, keys, 8);
-
-    delete tree;
-}
-
-void TestInsertion(std::vector<int> keys, std::vector<int> in_order_keys, Implementation impl) 
-{
-    AVLTree<int>* tree = nullptr;
-    if (impl == Implementation::Recursion)
-        tree = new RAVLTree<int>();
-
-    for (auto key : keys) {
+    for (auto key : source_keys)
+    {
         tree->Insert(key);
         EXPECT_EQ(tree->Query(key), true);
         EXPECT_EQ(tree->IsBalance(), true);
     }
 
-    CheckInOrder(tree, in_order_keys);
+    tree->Remove(reomve_key);
+    EXPECT_EQ(tree->Query(reomve_key), false);
+    source_in_order_keys.erase(std::find(source_in_order_keys.begin(), source_in_order_keys.end(), reomve_key));
+    CheckInOrder(tree, source_in_order_keys);
+    EXPECT_EQ(tree->IsBalance(), true);
+
+    delete tree;
+}
+
+
+void TestInsertion(std::vector<int> source_keys, std::vector<int> source_in_order_keys, Implementation impl) 
+{
+    EXPECT_EQ(source_keys.size(), source_in_order_keys.size());
+
+    AVLTree<int>* tree = nullptr;
+    if (impl == Implementation::Recursion)
+        tree = new RAVLTree<int>();
+
+    for (auto key : source_keys) {
+        tree->Insert(key);
+        EXPECT_EQ(tree->Query(key), true);
+        EXPECT_EQ(tree->IsBalance(), true);
+    }
+
+    CheckInOrder(tree, source_in_order_keys);
 
     delete tree;
 }
@@ -64,13 +68,38 @@ void TestInsertion(std::vector<int> keys, std::vector<int> in_order_keys, Implem
 void Test(Implementation impl)
 {
     TestInsertion({10, 6, 3}, {3, 6, 10}, impl);
-    TestInsertion({10, 6, 15, 3, 8, 7}, {3, 6, 7, 8, 10}, impl);
+    TestInsertion({10, 6, 15, 3, 8, 7}, {3, 6, 7, 8, 10, 15}, impl);
     TestInsertion({10, 15, 20}, {10, 15, 20}, impl);
     TestInsertion({10, 15, 8, 20, 14, 13}, {8, 10, 13, 14, 15, 20}, impl);
     TestInsertion({10, 6, 8}, {6, 8, 10}, impl);
     TestInsertion({10, 15, 12}, {10, 12, 15}, impl);
 
-    TestRemoval(impl);
+
+    // delete left child, right balance
+    TestRemoval({10, 6, 15, 3, 8, 13, 18, 7}, {3, 6, 7, 8, 10, 13, 15, 18}, 3, impl);
+
+    // delete left child, left rotate
+    TestRemoval({10, 6, 15, 3, 8, 13, 18, 9}, {3, 6, 8, 9, 10, 13, 15, 18}, 3, impl);
+
+    // delete right child, right rotate
+    TestRemoval({10, 6, 15, 3, 8, 13, 18, 12}, {3, 6, 8, 10, 12, 13, 15, 18}, 18, impl);
+
+    // delete right child, left balance
+    TestRemoval({10, 6, 15, 3, 8, 13, 18, 14}, {3, 6, 8, 10, 13, 14, 15, 18}, 18, impl);
+
+    // delete a node that no child
+    TestRemoval({10}, {10}, 10, impl);
+    TestRemoval({10, 6}, {6, 10}, 6, impl);
+    TestRemoval({10, 15}, {10, 15}, 15, impl);
+
+    // delete a node which has left child
+    TestRemoval({10, 6}, {6, 10}, 6, impl);
+
+    // delete a node which has right child
+    TestRemoval({10, 15}, {10, 15}, 15, impl);
+
+    // delete a node which has left child and right child
+    TestRemoval({10, 6, 15}, {10, 6, 15}, 10, impl);
 }
 
 
